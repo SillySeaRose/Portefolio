1bb74aee3fa58703daa9705044eaa19b7381ea6f
SillySeaRose <adrianosm99@gmail.com>
2023-10-27 16:08:42 +0100

    v1 Menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { Route, Routes} from 'react-router-dom';
 import MainPage from './Components/mPage/MainPage.js';
+import About from "./Components/about/About";
+import ArtPage from "./Components/artPage/ArtPage";
+import WebProject from "./Components/webProject/WebProject"
 
 function App() {
   return (
     <>
       <MainPage/>
+
       <Routes>
         <Route path='/' element={<MainPage/>}/>
+        <Route path='about' element={<About/>}></Route>
+        <Route path='artPage' element={<ArtPage/>}></Route>
+        <Route path='webProject' element={<WebProject/>}></Route>
       </Routes>
     </>
   );
--- src/Components/mPage/MainPage.js
+++ src/Components/mPage/MainPage.js
@@ -9,7 +9,13 @@ const mainPage = () => {
             <h1 className='NameTitle'>Adriano Moreira</h1>
             <h2 className='SubNameTitle'>Game Designer and Web Developer</h2>
 
-            <NavLink className="menuLink" to="/about">About</NavLink>
+            <nav id='menu'>
+              <ul id='organizer'>  
+                <li><NavLink className="menuLink" to="/about">About</NavLink></li>
+                <li><NavLink className="menuLink" to="/artPage">ArtPage</NavLink></li>
+                <li><NavLink className="menuLink" to="/webProject">WebProject</NavLink></li>
+              </ul>
+            </nav>
         </div>
     </>
   )
